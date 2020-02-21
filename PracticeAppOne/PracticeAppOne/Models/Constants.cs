18c00c2dc23f1a2fc4933b48f79d9e91263be5fb
Robert Flaherty <roberte.flaherty@Knights.ucf.edu>
2020-02-21 13:56:15 -0800

    2nd commit try

---
+++ PracticeAppOne/PracticeAppOne/Models/Constants.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PracticeAppOne.Models
+{
+    public class Constants
+    {
+        public static bool IsDev = true;
+
+    }
+}
---
+++ PracticeAppOne/PracticeAppOne/Views/LoginPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using PracticeAppOne.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PracticeAppOne.Models;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+
+namespace PracticeAppOne.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LoginPage : ContentPage
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+
+        void SignInProcedure(object sender, EventArgs e)
+        {
+            User user = new User(Entry_Username.Text, Entry_Password.Text);
+            if(user.CheckInformation())
+            {
+                DisplayAlert("Login","Login access","oke");
+            }
+            else
+            {
+                DisplayAlert("Login", "Login not correct, empty username or password", "oke");
+            }
+        }
+    }
+}
